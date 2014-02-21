a2b3d8ce9fe9d2d186f27b49ba12013619e329a5
Siriuse-R <admin@dejiplus.net>
2014-02-21 13:32:05 +0900

    Permissions Enum

---
+++ src/main/java/net/siriuser/dlobby/Perms.java
@@ -0,0 +1,25 @@
+/**
+ * Project : D-Lobby
+ * Package : net.siriuser.dlobby
+ * Created : 2014/02/21 - 13:01
+ */
+package net.siriuser.dlobby;
+
+import org.bukkit.permissions.Permissible;
+
+public enum Perms {
+    ;
+
+    final String HEADER = "dlobby.";
+    private String node;
+
+    Perms(final String node) {
+        this.node = HEADER + node;
+    }
+
+    public boolean has(final Permissible perm) {
+        if (perm == null)
+            return false;
+        return perm.hasPermission(node);
+    }
+}
