6e8a67af04a0e903ee02009aa74c2fd324066f91
Siriuse-R <admin@dejiplus.net>
2014-02-21 12:39:48 +0900

    BaseCodes.

---
+++ src/main/java/net/siriuser/dlobby/Lobby.java
@@ -0,0 +1,37 @@
+/**
+ * Project : D-Lobby
+ * Package : net.siriuser.dlobby
+ * Created : 2014/02/21 - 12:29
+ */
+package net.siriuser.dlobby;
+
+import net.syamn.utils.LogUtil;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lobby extends JavaPlugin {
+    private static Lobby instance;
+
+    @Override
+    public void onEnable() {
+        LogUtil.init(this);
+
+        PluginManager pm = getServer().getPluginManager();
+
+        PluginDescriptionFile pdfFile = this.getDescription();
+        LogUtil.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is enabled!");
+    }
+
+    @Override
+    public void onDisable() {
+        getServer().getScheduler().cancelTasks(this);
+
+        PluginDescriptionFile pdfFile = this.getDescription();
+        LogUtil.info(pdfFile.getName() + " version " + pdfFile.getVersion() + " is disabled!");
+    }
+
+    public static Lobby getInstance() {
+        return instance;
+    }
+}
